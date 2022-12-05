7a355249ddd8d66589118f5ec332eb69f777a856
csce410 <csce410@>
2022-12-04 20:19:40 -0600

    Updated code for MP7

--- MP7/file.C
+++ MP7/file.C
@@ -28,7 +28,16 @@
 
 File::File(FileSystem *_fs, int _id) {
     Console::puts("Opening file.\n");
-    assert(false);
+
+    fd = _id;
+    size = 0;
+
+    index = 1;
+    position = 0;
+
+    file_system = _fs;
+
+    //assert(false);
 }
 
 File::~File() {
@@ -43,20 +52,78 @@ File::~File() {
 
 int File::Read(unsigned int _n, char *_buf) {
     Console::puts("reading from file\n");
-    assert(false);
+
+    if (c_block == -1 || file_system == NULL) {
+        Console::puts("File not intialized, can not read \n");
+        return 0;
+    }
+
+
+    int read = 0;
+    int bytes_to_read = _n;
+
+    memset(block_cache,0,512);
+    
+    file_system->disk->read(c_block, (unsigned char *)block_cache);
+
+    while (!EoF() && (bytes_to_read > 0))
+    {
+        _buf[read] = block_cache[position];
+        bytes_to_read--;
+        read++; 
+        position++;
+
+   
+        
+    }
+
+   // assert(false);
+    return read;	
 }
 
 int File::Write(unsigned int _n, const char *_buf) {
     Console::puts("writing to file\n");
-    assert(false);
+
+    if (c_block == -1 || file_system == NULL) {
+        Console::puts("File not intialized, can not read \n");
+        return 0;
+    }
+
+
+     //Console::puts("passed buffer "); Console::puts(_buf);Console::puts("\n");
+    int write = 0;
+    int bytes_to_write = _n;
+
+
+    memset(block_cache,0,512);
+
+    file_system->disk->read(c_block, (unsigned char *)block_cache);
+    //Console::puts("position = "); Console::puti(position);
+    while (!EoF() && (bytes_to_write > 0))
+    {
+        block_cache[position] = _buf[write];
+        write++;
+        position++;
+        bytes_to_write--;
+
+    }
+
+    file_system->disk->write(c_block, block_cache);
+  //  assert(false);
 }
 
 void File::Reset() {
     Console::puts("resetting file\n");
     assert(false);
+    position = 0;
 }
 
 bool File::EoF() {
     Console::puts("checking for EoF\n");
     assert(false);
+    if(position > 512)
+	return true;
+
+    return false;
+
 }
--- MP7/file_system.C
+++ MP7/file_system.C
@@ -14,6 +14,9 @@
 
     /* -- (none) -- */
 
+
+#define NODES_PER_BLOCK (BLOCK_SIZE/size(m_node));
+
 /*--------------------------------------------------------------------------*/
 /* INCLUDES */
 /*--------------------------------------------------------------------------*/
@@ -29,6 +32,7 @@
 /* You may need to add a few functions, for example to help read and store 
    inodes from and to disk. */
 
+
 /*--------------------------------------------------------------------------*/
 /* CLASS FileSystem */
 /*--------------------------------------------------------------------------*/
@@ -39,13 +43,19 @@
 
 FileSystem::FileSystem() {
     Console::puts("In file system constructor.\n");
-    assert(false);
+    disk = NULL;
+    total_blocks = 0;
+    m_blocks = 0;
+    m_nodes = 0;
+    size = 0;
+    //assert(false);
 }
 
 FileSystem::~FileSystem() {
     Console::puts("unmounting file system\n");
+    
     /* Make sure that the inode list and the free list are saved. */
-    assert(false);
+    //assert(false);
 }
 
 
@@ -56,10 +66,18 @@ FileSystem::~FileSystem() {
 
 bool FileSystem::Mount(SimpleDisk * _disk) {
     Console::puts("mounting file system from disk\n");
-
+    if (disk == NULL)
+    {
+	disk = _disk;
+	return true;
+    }
+    else
+    {
+	return false;
+    }
     /* Here you read the inode list and the free list into memory */
     
-    assert(false);
+    //assert(false);
 }
 
 bool FileSystem::Format(SimpleDisk * _disk, unsigned int _size) { // static!
@@ -67,13 +85,91 @@ bool FileSystem::Format(SimpleDisk * _disk, unsigned int _size) { // static!
     /* Here you populate the disk with an initialized (probably empty) inode list
        and a free list. Make sure that blocks used for the inodes and for the free list
        are marked as used, otherwise they may get overwritten. */
+    disk = _disk;
+    size = _size;
+    
+    total_blocks = ((FileSystem::size)/Simple_disk::BLOCK_SIZE) + 1;
+    m_nodes = (total_blocks/BLOCK_LIMIT) + 1;
+    m_blocks = (m_nodes * sizeof(m_node)) / BLOCK_SIZE + 1;
+
+ 
+    for (int j = 0; j < (total_blocks/8); j++)
+    {
+	block_map[j] = 0;
+    }
+
+    int i;    
+    for (i = 0; i < (m_blocks/8); i++) 
+    {
+	block_map[i] = 0xFF;
+    }
+
+    block_map[i] = 0;
+
+
+   for (int j = 0; j < (m_blocks%8); j++)
+   {
+	block_map[i] = block_map[i] | (1<<j);
+   }
+
+
+   char buf[512];
+   memset(buf, 0, 512);
+   for(int j = 0; j < total_blocks; j++)
+   {
+	disk->write(j, (unsigned char *)buf);
+   }
+    
     assert(false);
 }
 
-Inode * FileSystem::LookupFile(int _file_id) {
+File * FileSystem::LookupFile(int _file_id) {
     Console::puts("looking up file with id = "); Console::puti(_file_id); Console::puts("\n");
+
+    File* file = (File *) new File();
+
+    char buf[512];
+
+    memset(buf, 0, 512);
+
+    for(int i=0; i < m_blocks; i++)
+    {
+	memset(buf, 0, 512);
+        disk->read(i, (unsigned char *)buf);
+ 	m_node* m_node_l = (m_node *)buf;
+
+	for(int j=0;j < NODES_PER_BLOCK;j++)
+	{
+		if(m_node_l[j].fd == _file_id)
+		{
+			file->fd       = _file_id;
+			file->size     = m_node_l[j].size;
+			file->c_block  = m_node_l[j].block[0];
+			file->index    = 1;
+			file->position = 0;
+
+			for(int k=0; k < BLOCK_LIMIT; k++)
+			{
+				file->blocks[k] = m_node_l[j].block[k];
+			}
+
+			file->file_system = FILE_SYSTEM;
+			if (file->file_system == NULL)
+			{
+                    		Console::puts("File system NULL in lookup");Console::puts("\n");
+			}
+
+	                Console::puts("Found file with id ");Console::puti(_file_id);Console::puts("\n");
+        	        return file;				
+
+		}
+	}
+
+    }
+
     /* Here you go through the inode list to find the file. */
-    assert(false);
+    //assert(false);
+    return NULL;
 }
 
 bool FileSystem::CreateFile(int _file_id) {
@@ -81,7 +177,43 @@ bool FileSystem::CreateFile(int _file_id) {
     /* Here you check if the file exists already. If so, throw an error.
        Then get yourself a free inode and initialize all the data needed for the
        new file. After this function there will be a new file on disk. */
-    assert(false);
+
+    if (LookupFile(_file_id) != NULL)
+    {
+        Console::puts("File already exists with this id, choose new id\n");
+        return false;
+    }
+
+
+    char buf[512];
+    memset(buf, 0, 512);
+
+    for (int i = 0; i < m_blocks; i++) 
+    {
+
+        memset(buf, 0, 512);        //set the buffer to 0, to be used in reading the disk.
+        disk->read (i, (unsigned char *)buf);
+        m_node* m_node_l = (m_node *) buf;
+
+        for (int j = 0; j < NODES_PER_BLOCK; j++) 
+	{
+            if (m_node_l[j].fd == 0) 
+	    {
+                m_node_l[j].fd = _file_id;
+
+                m_node_l[j].block[0] = GetFreeBlock();
+                Console::puts("get block "); Console::puti(m_node_l[j].block[0]);
+                m_node_l[j].b_size   = 1;
+
+                disk->write(i, (unsigned char *)buf);
+                return true;
+            }
+        }
+    }
+	
+    return false;
+
+    //assert(false);
 }
 
 bool FileSystem::DeleteFile(int _file_id) {
@@ -89,4 +221,74 @@ bool FileSystem::DeleteFile(int _file_id) {
     /* First, check if the file exists. If not, throw an error. 
        Then free all blocks that belong to the file and delete/invalidate 
        (depending on your implementation of the inode list) the inode. */
+
+
+    char buf[512];
+    memset(buf,0,512);
+
+    for (int i = 0; i<m_blocks; i++)
+    {
+
+
+        memset(buf, 0, 512);        //set the buffer to 0, to be used in reading the disk.
+        disk->read (i, (unsigned char *)buf);
+        m_node* m_node_l = (m_node *) buf;
+
+        for (int j = 0; j < NODES_PER_BLOCK; j++) {
+            if (m_node_l[j].fd == _file_id) {
+                m_node_l[j].fd = 0;
+                m_node_l[j].size = 0;
+                m_node_l[j].b_size = 0;
+                for (int k = 0; k < BLOCK_LIMIT; k++) {
+                    if (m_node_l[j].block[k] != 0) {
+                        FreeBlock(m_node_l[j].block[k]);
+                    }
+                    m_node_l[j].block[k] = 0;
+                }
+                disk->write(i, (unsigned char *)buf);
+                return true;
+            }
+        }
+    }
+
 }
+
+
+
+int FileSystem::GetFreeBlock() {
+
+    // We need to reserve the first m_blocks in the disk for file system management.
+    //TODO
+
+    Console::puts("Total blocks "); Console::puti(total_blocks/8);Console::puts("\n");
+
+    for (int i = 0; i < (total_blocks / 8); i++) {
+        if (block_map[i] != 0xFF) {
+            for (int j = 0; j < 8; j++) {
+                if (block_map[i] & (1 << j)) {
+                    continue;
+                } else {
+                    block_map[i] = block_map[i] | (1 << j);
+                    int b= j + i*8;
+                    Console::puts("Allocating block number");Console::puti(b);Console::puts("\n");
+                    return b;
+                }
+            }
+        }
+    }
+    Console::puts("returning block 0\n");
+    return 0;
+}
+
+
+void FileSystem::FreeBlock(int block_no)
+{
+	int node = block_no / 8;
+	int index = block_no % 8;
+
+    block_map[node] = block_map[node] | (1 << index) ;
+    block_map[node] = block_map[node] ^ (1 << index) ;
+
+}
+
+
